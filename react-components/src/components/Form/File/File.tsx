66091c3a8cf79633996d0bb313f632d5785a9059
Evgen <yevgenchikk@mail.ru>
2023-03-23 22:58:31 +0300

    feat: add file input

--- react-components/src/components/Form/File/File.tsx
+++ react-components/src/components/Form/File/File.tsx
@@ -1,13 +1,20 @@
 /* eslint-disable react/prefer-stateless-function */
-import { Component } from 'react';
+import { Component, LegacyRef } from 'react';
 import styles from './File.module.scss';
 
-class File extends Component {
+class File extends Component<{ fileRef: LegacyRef<HTMLInputElement> }> {
+  ref: LegacyRef<HTMLInputElement>;
+
+  constructor(props: { fileRef: LegacyRef<HTMLInputElement> }) {
+    super(props);
+    this.ref = props.fileRef;
+  }
+
   render() {
     return (
       <label className={styles.file} htmlFor="icon">
         Сover:
-        <input name="icon" type="file" title=" " />
+        <input ref={this.ref} name="icon" type="file" title=" " />
       </label>
     );
   }
--- react-components/src/components/Form/Form.tsx
+++ react-components/src/components/Form/Form.tsx
@@ -19,7 +19,7 @@ class Form extends Component<{ addCard: (cards: ICard) => void }> {
 
   inputCheckbox: LegacyRef<HTMLInputElement>;
 
-  // inputFile = createRef();
+  inputFile: LegacyRef<HTMLInputElement>;
 
   inputSelect: LegacyRef<HTMLSelectElement>;
 
@@ -31,27 +31,25 @@ class Form extends Component<{ addCard: (cards: ICard) => void }> {
     this.inputRadio = createRef<HTMLInputElement>();
     this.inputCheckbox = createRef<HTMLInputElement>();
     this.inputSelect = createRef<HTMLSelectElement>();
+    this.inputFile = createRef<HTMLInputElement>();
   }
 
   // eslint-disable-next-line class-methods-use-this
   handleSubmit = (e: FormEvent) => {
     e.preventDefault();
 
+    console.log(URL.createObjectURL(this.inputFile.current?.files?.[0]));
+
     const card = {
-      name: this.inputText.current.value as string,
-      date: this.inputDate.current.value as string,
-      category: this.inputRadio.current.value as string,
-      consent: this.inputCheckbox.current.value as string,
-      language: this.inputSelect.current.value as string,
+      // name: this.inputText.current.value as string,
+      // date: this.inputDate.current.value as string,
+      // category: this.inputRadio.current.checked as string,
+      // consent: this.inputCheckbox.current.checked as string,
+      // language: this.inputSelect.current.value as string,
+      icon: this.inputFile?.current?.value as string,
     };
 
     this.props.addCard(card);
-
-    // console.log(this.inputText.current.value);
-    // console.log(this.inputDate.current.value);
-    // console.log(this.inputRadio.current.value);
-    // console.log(this.inputCheckbox.current.value);
-    // console.log(this.inputSelect.current.value);
   };
 
   render() {
@@ -62,6 +60,7 @@ class Form extends Component<{ addCard: (cards: ICard) => void }> {
         <Radio inputRef={this.inputRadio} />
         <Checkbox inputRef={this.inputCheckbox} />
         <Select selectRef={this.inputSelect} />
+        <File fileRef={this.inputFile} />
         <Button addCard={(card: ICard) => this.props.addCard(card)} />
       </form>
     );
