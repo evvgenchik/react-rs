41b67447d8f3fb9c2fa1999d9980cd0c80d703f6
Evgen <yevgenchikk@mail.ru>
2023-03-27 23:36:48 +0300

    refactor: unite some  test code

--- react-components/src/pages/FormPage/FormPage.test.tsx
+++ react-components/src/pages/FormPage/FormPage.test.tsx
@@ -3,37 +3,43 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import FormPage from './FormPage';
 
+const fillForm = async () => {
+  const title = screen.getByRole('textbox', {
+    name: 'Title: Description: Language: JavaScript',
+  });
+  const description = screen.getByRole('textbox', { name: '' });
+  const format = screen.getAllByRole('radio');
+  const date = screen.getByTestId('input-date');
+  const icon = screen.getByTestId('input-file');
+  const agreement = screen.getByRole('checkbox');
+  const mockFile = new File(['JS'], 'book.png', {
+    type: 'image/png',
+  });
+  const dateExample = '2022-09-09';
+
+  await waitFor(async () => {
+    await userEvent.type(title, 'JS');
+    await userEvent.type(description, 'about every feature');
+    await userEvent.clear(date);
+    await userEvent.type(date, dateExample);
+    await userEvent.click(format[0]);
+    await userEvent.click(agreement);
+    fireEvent.change(icon, {
+      target: { files: [mockFile] },
+    });
+  });
+};
+
 describe('FormPage', () => {
   it('Check card add', async () => {
     render(<FormPage />);
-
     const form = screen.getByTestId('formAddCard');
-    const title = screen.getByRole('textbox', {
-      name: 'Title: Description: Language: JavaScript',
-    });
-    const description = screen.getByRole('textbox', { name: '' });
-    const format = screen.getAllByRole('radio');
-    const date = screen.getByTestId('input-date');
-    const icon = screen.getByTestId('input-file');
-    const agreement = screen.getByRole('checkbox');
-    const mockFile = new File(['JS'], 'book.png', {
-      type: 'image/png',
-    });
-    const dateExample = '2022-09-09';
-
-    await waitFor(() => userEvent.type(title, 'JS'));
-    await waitFor(() => userEvent.type(description, 'about every feature'));
-    await waitFor(() => userEvent.clear(date));
-    await waitFor(() => userEvent.type(date, dateExample));
-    await waitFor(() => userEvent.click(format[0]));
-    await waitFor(() => userEvent.click(agreement));
-    await waitFor(() =>
-      fireEvent.change(icon, {
-        target: { files: [mockFile] },
-      })
-    );
-
+    await fillForm();
     await waitFor(() => fireEvent.submit(form));
-    expect(screen.getByRole('listitem')).toBeInTheDocument();
+
+    const card = screen.getByRole('listitem');
+    expect(card).toBeInTheDocument();
   });
 });
+
+export default fillForm;
