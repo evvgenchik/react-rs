7c7934b6cf9bc32c01f812e952b593b22b46f957
Evgen <yevgenchikk@mail.ru>
2023-03-29 15:47:02 +0300

    feat: add function component on FormPage

--- react-components/src/pages/FormPage/FormPage.tsx
+++ react-components/src/pages/FormPage/FormPage.tsx
@@ -1,29 +1,21 @@
-import { Component } from 'react';
+import { FC, useState } from 'react';
 import Form from '../../components/Form/Form';
 import CardList from '../../components/CardList/CardList';
 import { ICard } from '../../utils/types';
 
-class FormPage extends Component<object, { cards: ICard[] }> {
-  constructor(props: object) {
-    super(props);
-    this.state = { cards: [] };
-  }
+const FormPage: FC = () => {
+  const [cardsArr, setCardsArr] = useState<ICard[]>([]);
 
-  addCard(card: ICard) {
-    this.setState((prevState: { cards: ICard[] }) => {
-      return { cards: [card, ...prevState.cards] };
-    });
-  }
+  const addCard = (card: ICard) => {
+    setCardsArr([card, ...cardsArr]);
+  };
 
-  render() {
-    const { cards } = this.state;
-    return (
-      <div>
-        <Form addCard={(card: ICard) => this.addCard(card)} />
-        {cards.length > 0 && <CardList cards={cards} />}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Form addCard={addCard} />
+      {cardsArr.length > 0 && <CardList cards={cardsArr} />}
+    </div>
+  );
+};
 
 export default FormPage;
