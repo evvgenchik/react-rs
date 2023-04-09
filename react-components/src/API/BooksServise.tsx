751342f97927551604c9382434287e22b0cbca31
Evgen <yevgenchikk@mail.ru>
2023-04-09 13:02:54 +0300

    fix: eslint warinig useEffect

--- react-components/src/API/BooksServise.tsx
+++ react-components/src/API/BooksServise.tsx
@@ -2,14 +2,18 @@ import { ICard } from '../utils/types';
 
 export default class BooksServise {
   static async getAll() {
-    const res = await fetch('http://localhost:3333/books');
+    const res = await fetch(
+      'https://json-server-production-2471.up.railway.app/books'
+    );
     if (res.status !== 200) throw new Error(`Status code error: ${res.status}`);
     const jsonData: ICard[] = await res.json();
     return jsonData;
   }
 
   static async getSpecific(param: string) {
-    const res = await fetch(`http://localhost:3333/books?q=${param}`);
+    const res = await fetch(
+      `https://json-server-production-2471.up.railway.app/books?q=${param}`
+    );
     if (res.status !== 200) throw new Error(`Status code error: ${res.status}`);
     const jsonData: ICard[] = await res.json();
     return jsonData;
--- react-components/src/pages/Catalog/Catalog.tsx
+++ react-components/src/pages/Catalog/Catalog.tsx
@@ -1,29 +1,38 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Search from '../../components/Search/Search';
 import CardList from '../../components/CardList/CardList';
 import BooksServise from '../../API/BooksServise';
 import { ICard } from '../../utils/types';
 import Loader from '../../components/UI/Loader/Loader';
-import useFetching from '../../hooks/useFetch';
 
 function Catalog() {
   const [books, setBooks] = useState<ICard[]>();
+  const [loading, setloading] = useState<boolean>();
+  const [errorMessage, setError] = useState<string>('');
 
-  const [fetchApi, loading, errorMessage] = useFetching(async () => {
+  const fethcData = async () => {
     const searchParams = localStorage.getItem('search');
     const booksFromServer = searchParams
       ? await BooksServise.getSpecific(searchParams)
       : await BooksServise.getAll();
     if (booksFromServer) setBooks(booksFromServer);
-  }) as [() => Promise<void>, boolean, string];
-
-  const fetchData = useCallback(() => {
-    return fetchApi;
-  }, [fetchApi]);
+  };
 
   useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+    const fetchApi = async () => {
+      try {
+        setloading(true);
+        setError('');
+        await fethcData();
+      } catch (error) {
+        const errorMess = error as Error;
+        setError(errorMess.message);
+      } finally {
+        setloading(false);
+      }
+    };
+    fetchApi();
+  }, []);
 
   return (
     <>
