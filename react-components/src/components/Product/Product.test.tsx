530e4287ed161207a94d369bfa0bbc4b4d5e847e
Evgen <yevgenchikk@mail.ru>
2023-04-09 13:41:33 +0300

    fix: product test

--- react-components/src/components/Product/Product.test.tsx
+++ react-components/src/components/Product/Product.test.tsx
@@ -1,11 +1,14 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { render, screen, within } from '@testing-library/react';
 import Product from './Product';
 import booksJson from '../../data/booksDB.json';
 
 describe('Product', () => {
   it('Renders catalog component', () => {
-    render(<Product book={booksJson.books[0]} />);
+    const cardClickHandler = vi.fn();
+    render(
+      <Product cardClickHandler={cardClickHandler} book={booksJson.books[0]} />
+    );
     const item = screen.getByRole('listitem', {});
     expect(within(item).getByRole('img')).toBeInTheDocument();
   });
