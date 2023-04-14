65e1c2d50e0a40916696e81504c5a4bb3c17ab46
Evgen <yevgenchikk@mail.ru>
2023-04-14 14:49:11 +0300

    refactor: links in api file

--- react-components/src/API/BooksServise.tsx
+++ react-components/src/API/BooksServise.tsx
@@ -1,19 +1,17 @@
 import { ICard } from '../utils/types';
 
 export default class BooksServise {
+  static url = 'http://localhost:3333/books';
+
   static async getAll() {
-    const res = await fetch(
-      'https://json-server-production-2471.up.railway.app/books'
-    );
+    const res = await fetch(this.url);
     if (res.status !== 200) throw new Error(`Status code error: ${res.status}`);
     const jsonData: ICard[] = await res.json();
     return jsonData;
   }
 
   static async getSpecific(param: string) {
-    const res = await fetch(
-      `https://json-server-production-2471.up.railway.app/books?q=${param}`
-    );
+    const res = await fetch(`${this.url}?q=${param}`);
     if (res.status !== 200) throw new Error(`Status code error: ${res.status}`);
     const jsonData: ICard[] = await res.json();
     return jsonData;
